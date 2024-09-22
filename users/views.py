29acf3cdc3180c122953f50f2f4bc6e0b97ad0c1
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-09-22 22:02:01 +0300

    feat: add registration and login views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.urls import reverse
+from django.views.generic import CreateView
 
-# Create your views here.
+from users.forms import LoginUserForm, RegisterUserForm
+
+
+class LoginUser(LoginView):
+    template_name = 'users/signin.html'
+    form_class = LoginUserForm
+
+    extra_context = {
+        'title': 'Sign In',
+    }
+
+
+class RegisterUser(CreateView):
+    template_name = 'users/signup.html'
+    form_class = RegisterUserForm
+
+    extra_context = {
+        'title': 'Sign Up',
+    }
