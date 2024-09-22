108fe8a64d2878fc47e9b60327b9d8c5cbd806ea
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-09-22 22:03:22 +0300

    feat: add location model

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length=255)
+    users_id = models.ManyToManyField(User, related_name='locations')
+    lat = models.DecimalField(max_digits=5, decimal_places=2)
+    lon = models.DecimalField(max_digits=5, decimal_places=2)
+
+    class Meta:
+        unique_together = (('lat', 'lon'),)
+
+    def __str__(self):
+        return f"{self.name}: lat: {self.lat}, lon: {self.lon}"
