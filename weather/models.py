d48f61247105ee9426406d3739ed391180cfdc7e
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-10-20 23:46:01 +0300

    feat: add country field to Location model

--- weather/models.py
+++ weather/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Location(models.Model):
     name = models.CharField(max_length=255)
+    country = models.CharField(max_length=5)
     users_id = models.ManyToManyField(User, related_name='locations')
     lat = models.DecimalField(max_digits=5, decimal_places=2)
     lon = models.DecimalField(max_digits=5, decimal_places=2)
