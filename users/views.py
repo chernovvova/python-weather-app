e8854b152ad4cae1125404742b74a6c8aff85317
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-10-20 23:27:24 +0300

    feat: remove unused imports

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.views import LoginView
-from django.urls import reverse
+from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 from users.forms import LoginUserForm, RegisterUserForm
